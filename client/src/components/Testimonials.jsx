f552448be075b52644a49d0afeee1397e496beed
Imtiaz_1 <smimtiaz58@gmail.com>
2024-11-20 19:29:59 +0600

    2nd commit from PC

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -10,14 +10,17 @@ const Testimonials = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-4xl mx-auto px-4 py-8 ">
         {testimonialsData.map((item) => (
-          <div key={item.id}>
-            <p>&quot;</p>
-            <p>{item.text}</p>
-            <div>
-              <img src={item.image} alt="" />
+          <div
+            key={item.id}
+            className="bg-white rounded-xl p-6 drop-shadow-md max-w-lg m-auto hover:scale-105 transition-all duration-700"
+          >
+            <p className="text-4xl text-gray-500">&quot;</p>
+            <p className="text-sm text-gray-500">{item.text}</p>
+            <div className="flex items-center gap-3 mt-5">
+              <img src={item.image} alt="" className="w-9 rounded-full" />
               <div>
                 <p>{item.author}</p>
-                <p>{item.jobTitle}</p>
+                <p className="text-sm text-gray-600">{item.jobTitle}</p>
               </div>
             </div>
           </div>
---
+++ client/src/components/Upload.jsx
@@ -0,0 +1,9 @@
+const Upload = () => {
+  return (
+    <div>
+      <h1>See the magic, Try now</h1>
+    </div>
+  );
+};
+
+export default Upload;
