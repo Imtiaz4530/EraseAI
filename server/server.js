708b6918e7092946ed1b5d6a158ec40afcc0f744
Imtiaz4530 <smimtiaz58@gmail.com>
2024-11-21 19:42:57 +0600

    webhook added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 
 import connectDB from "./configs/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 //App Config
 const PORT = process.env.PORT || 4000;
@@ -12,10 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 connectDB();
+
 //App routes
 app.get("/", (req, res) => {
   res.send("API WORKING.");
 });
+
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
